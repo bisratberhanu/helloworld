a14a52f2d4af024373c4800ff9a6eb5753af1550
khalinoid <khalinoid@gmail.com>
2024-09-03 12:22:26 +0300

    new shortcode on public folder created

---
+++ public/controller/greetings.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * The public-facing functionality of the plugin.
+ *
+ * @link       https://bisratberhanu.me
+ * @since      1.0.0
+ *
+ * @package    Helloworld
+ * @subpackage Helloworld/public
+ */
+
+/**
+ * The public-facing functionality of the plugin.
+ *
+ * Defines the plugin name, version, and two examples hooks for how to
+ * enqueue the public-facing stylesheet and JavaScript.
+ *
+ * @package    Helloworld
+ * @subpackage Helloworld/public
+ * @author     bisrat <bisratbnegus@gmail.com>
+ */
+class Helloworld_Public_Greetings {
+
+    public function helloworld_greetings(){
+        ob_start();
+
+        echo "Hello world";
+
+		include_once plugin_dir_path( dirname( __FILE__ ) ) . 'partials/front/index.php';
+
+
+        return ob_get_clean();
+    }
+}
